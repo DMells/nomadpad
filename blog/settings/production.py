6d1bf870af43d64b7b9aa8398b83648ce6626ee0
DMells <dave@davemellor.com>
2018-02-22 18:56:10 +0700

    Variable protection

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -5,12 +5,14 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['www.nomadpad.io']
 
+SECRET_KEY = config('SECRET_KEY')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'nomadpaddb',
         'USER': 'super',
-        'PASSWORD': 'lalalala',
+        'PASSWORD': config('DB_PASSWORD'),
         'HOST': 'DMells123-678.postgres.pythonanywhere-services.com',
         'PORT':'10678',
     }
