c85d8ea4d2d056d40fc42abb2f6835183e9e761e
DMells <dave@davemellor.com>
2018-02-23 15:17:32 +0700

    commit

--- blog/settings/base.py
+++ blog/settings/base.py
@@ -31,7 +31,7 @@ CKEDITOR_CONFIGS = {
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.config('SECRET_KEY')
 
 
 
