068cbc7984f4df1aceea2b4e6e987c8bcef638fd
DMells <dave@davemellor.com>
2018-02-23 19:32:05 +0700

    c

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -1,4 +1,5 @@
 import environ
+import Path
 # https://github.com/wooyek/django-settings-strategy
 environ.Env.read_env(Path(__file__) / "production.env", DEBUG='False', ASSETS_DEBUG='False')
 from .base import *
