53494150493e982d4945b0e12b31f2da5edfa9b4
DMells <dave@davemellor.com>
2018-02-24 20:33:02 +0700

    c

--- blog/settings/base.py
+++ blog/settings/base.py
@@ -41,15 +41,16 @@ CKEDITOR_CONFIGS = {
 }
 
 DEBUG = config('DEBUG', default=False, cast=bool)
-
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = ["^@(z7s#9imf*xl+j%@i#%ftx_2mmb#%or(xnrccidj19&%c3(8"]
+# SECRET_KEY = config('SECRET_KEY')
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'nomadpaddb',
         'USER': 'super',
-        'PASSWORD': config('DB_PASSWORD'),
+        'PASSWORD': 'lalalala',
+        # 'PASSWORD': config('DB_PASSWORD'),
         'HOST': 'DMells123-678.postgres.pythonanywhere-services.com',
         'PORT':'10678',
     }
