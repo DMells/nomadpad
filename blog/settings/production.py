08491a9b035f3a2ebda60ea8e86b53c527142677
DMells <dave@davemellor.com>
2018-02-25 14:20:00 +0700

    cc

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -10,6 +10,6 @@ ALLOWED_HOSTS = ['www.nomadpad.io']
 
 ########## SECRET CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = ["^@(z7s#9imf*xl+j%@i#%ftx_2mmb#%or(xnrccidj19&%c3(8"]
+
 # SECRET_KEY= config('SECRET_KEY')
 ########## END SECRET CONFIGURATION
