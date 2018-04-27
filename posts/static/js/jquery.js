a7a9c5af95bfb9231e59c3d27da53166e15ecb2d
DMells <dave@davemellor.com>
2018-04-27 15:07:31 +0700

    jquerytoggle

--- posts/static/js/jquery.js
+++ posts/static/js/jquery.js
@@ -2,19 +2,29 @@ $(document).ready(function() {
     var contentPlacement = $('#header').position().top + $('#header').height()
     $('#content').css('margin-top',contentPlacement);
 
-// NOTE : adding ; at the end of the actual toggle line makes it
-// not work! 
+
 // Clicking toggle button will slide down menu bar
-    $("#navbar").click( function(event){
-        $("#mynavbar").toggleClass('responsive')
+    $("#navbartoggler").click( function(event){
+        $("#navbaractual").slideToggle()
     });
-// If user clicks anywhere that isnt the dropdown button or the actual 
-// navbar, navbar will disappear
-	$(document).click( function(event){
-    	if($(event.target).is("#mynavbar, #navbar") === false) {
-    		$("#mynavbar").removeClass('responsive')
-    	}
+
+// // If user clicks anywhere that isnt the dropdown button or the actual 
+// // navbar, navbar will disappear
+     $(document).click( function(event)
+        {
+         if($("#navbaractual").is(':visible'))
+            {
+            if($(event.target).is("#navbaractual,  #navbartoggler") === false) 
+                {
+                $("#navbaractual").slideToggle()
+                }
+            }
+        });
+    
+
+
+
+
+
     });
- 
- });
 
